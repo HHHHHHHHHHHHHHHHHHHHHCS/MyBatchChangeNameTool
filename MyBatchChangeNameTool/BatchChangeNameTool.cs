c32853d81d8b003c9572f7e60ea9cb65734b66cd
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-08-06 19:28:49 +0800

    Initial commit

---
+++ MyBatchChangeNameTool/BatchChangeNameTool.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBatchChangeNameTool
+{
+    public class BatchChangeNameTool
+    {
+        public const string _inputPath = "input";
+        public const string _outputPath = "output";
+
+        public static readonly string inputPath, outputPath;
+        
+        static BatchChangeNameTool()
+        {
+            inputPath =Path.Combine(Directory.GetCurrentDirectory() , _inputPath);
+            outputPath = Path.Combine(Directory.GetCurrentDirectory() , _outputPath);
+        }
+
+        public void ChangeName(string format)
+        {
+            Directory.CreateDirectory(inputPath);
+            Directory.CreateDirectory(outputPath);
+
+            var filesPath =  Directory.GetFiles(inputPath);
+
+            int index = 0;
+            foreach (var item in filesPath)
+            {
+                if(index==0)
+                {
+                    format +=  Path.GetExtension(item);
+                }
+
+                string outPath = Path.Combine(outputPath, string.Format(format, index));
+                File.Copy(item, outPath, true);
+                index++;
+            }
+
+        }
+
+    }
+}
---
+++ MyBatchChangeNameTool/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBatchChangeNameTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BatchChangeNameTool tool = new BatchChangeNameTool();
+            string input = Console.ReadLine();
+            if(string.IsNullOrEmpty(input))
+            {
+                input = "s{0:D4}";
+            }
+            tool.ChangeName(input);
+        }
+    }
+}
